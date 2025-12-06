58aa458c3490d244d40dad1410a9026adcb00186
Reinier van Elderen <reiniervanelderen1@gmail.com>
2025-12-06 15:06:12 +0100

    partial part 5

--- day_5/part_2.py
+++ day_5/part_2.py
@@ -2,11 +2,29 @@ input_file = "input"
 
 
 def in_range_other(ranges, r):
+    parts = r.split("-")
     for r1 in ranges:
-        parts = r.split('-')
-        parts1 = r1.split('-')
+
+        parts1 = r1.split("-")
         # if in other range
-        if int(parts[0]) > int(parts1[0]) and int(parts[1]) < int(parts1[1]):
+        if int(parts[0]) <= int(parts1[0]) and int(parts[1]) >= int(parts1[1]):
+            return True
+    return False
+
+
+def in_range_other2(ranges, r):
+    parts = r.split("-")
+    for r1 in ranges:
+
+        parts1 = r1.split("-")
+        print(parts, parts1)
+        # if in other range
+        if (
+            int(parts[0]) >= int(parts1[0])
+            and int(parts[1]) <= int(parts1[1])
+            and parts != parts1
+        ):
+            print("test")
             return True
     return False
 
@@ -28,22 +46,56 @@ with open(input_file, "r") as f:
 print(ranges)
 
 r_new = []
-fresh = set()
+
+final_ranges = set()
 # remove ranges in other range
-for ingredient in ingredients:
-    for i, r in enumerate(ranges):
-        if in_range_other(ranges, r):
-            ranges.pop(i)
+for r1 in ranges:
+    r1_bounds = r1.split("-")
+    max_left_bound = int(r1_bounds[0])
+    max_right_bound = int(r1_bounds[1])
+    used = []
+    for r2 in ranges:
+        if in_range_other(final_ranges, r1):
+            break
+        r2_bounds = r2.split("-")
+        left_bound = int(r2_bounds[0])
+        right_bound = int(r2_bounds[1])
+        print(
+            f"max {max_left_bound}, {max_right_bound}, new {left_bound}, {right_bound} "
+        )
+        if max_left_bound < left_bound and right_bound < max_right_bound:  # in range
+            break
 
+        if (
+            left_bound < max_left_bound and left_bound < max_left_bound < right_bound
+        ):  # new left bound
+            max_left_bound = left_bound
 
-print(ranges)
+        elif (
+            left_bound < max_right_bound > max_left_bound
+            and max_right_bound < right_bound
+        ):  # new right bound
+            max_right_bound = right_bound
 
-fresh = set()
-for ingredient in ingredients:
-    for r in ranges:
-        parts = r.split('-')
-        fresh.update(range(int(parts[0]), int(parts[1])+1))
-        # for n in range(int(parts[0]), int(parts[1])+1):
-        #     fresh.add(n)
+        elif (
+            max_left_bound > left_bound and max_right_bound < right_bound
+        ):  # both in range
+            max_left_bound = left_bound
+            max_right_bound = right_bound
 
-print(len(fresh))
+    final_ranges.add(f"{max_left_bound}-{max_right_bound}")
+
+print(final_ranges)
+final_final_ranges = []
+for r in final_ranges:
+    print(r)
+    if not in_range_other2(final_ranges, r):
+        final_final_ranges.append(r)
+print(final_final_ranges)
+
+total = 0
+for r in final_final_ranges:
+    r_split = r.split("-")
+    total += (int(r_split[1]) - int(r_split[0])) + 1
+
+print(total)
